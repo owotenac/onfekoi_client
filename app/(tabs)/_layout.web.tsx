cacd2578c734999a52dc50405617635ebd4385e7
owoignier <owoignier@slb.com>
2026-02-23 18:45:19 +0100

    overflow

--- app/(tabs)/_layout.web.tsx
+++ app/(tabs)/_layout.web.tsx
@@ -1,23 +1,29 @@
 import { AntDesign, Entypo } from '@expo/vector-icons';
 import { router, Tabs } from 'expo-router';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from 'react-native';
+
 
 function WebNavBar() {
+
+  const { width } = useWindowDimensions();
+const showText = width >= 400;
+
+
   return (
     <View style={styles.navbar}>
       <View style={styles.navLinks}>
         <TouchableOpacity onPress={() => router.replace('/(tabs)')} style={styles.navItem}>
           <AntDesign name="home" size={34} color='white' />
-          <Text style={styles.navText}>Home</Text>
+           {showText && <Text style={styles.navText}>Home</Text>}
         </TouchableOpacity>
         <TouchableOpacity onPress={() => router.replace('/favorites_screen')} style={styles.navItem}>
           <Entypo name="heart" size={34} color='white' />
-          <Text style={styles.navText}>Favorites</Text>
+          {showText && <Text style={styles.navText}>Favorites</Text>}
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => router.replace('/profile')} style={styles.navItem}>
           <Entypo name="user" size={34} color='white' />
-          <Text style={styles.navText}>Profile</Text>
+          {showText && <Text style={styles.navText}>Profile</Text>}
         </TouchableOpacity>
 
       </View>
@@ -46,10 +52,10 @@ export default function WebTabLayout() {
 
 const styles = StyleSheet.create({
   navbar: {
-    flex: 0.1,
-    flexDirection: 'row',
+    //flex: 0.1,
+    //flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    //alignItems: 'center',
     padding: 16,
     backgroundColor: "#15151D",
   },
